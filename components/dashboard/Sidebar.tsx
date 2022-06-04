d68dd08939f80a5f1e28cc5706e5c02fbd584b89
TheLDB <ldb@erikboles.com>
2022-06-04 01:33:12 -0600

    Im making the sidebar dothings :D

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -10,7 +10,41 @@ const Sidebar = (props: ISidebarProps) => {
 			<div
 				className={`${
 					props.theme === "dark" ? "gradient-border" : "gradient-border-light"
-				} w-full h-full rounded-3xl drop-shadow-xl`}></div>
+				} w-full h-full rounded-3xl drop-shadow-xl flex flex-col`}>
+
+                    {/* Logo Mark */}
+                    <div className="w-full h-1/8 flex justfiy-center items-center">
+                        {props.theme === "dark" && <img src="/m-white.png" className="scale-50" />}
+                        {props.theme === "light" && <img src="/m-grad.png" className="scale-50" />}
+                    </div>
+
+                    {/* Tabs */}
+                    <div className="w-full h-6/8 bg-blue-500 flex flex-col">
+                        <div className="Home w-full h-1/6 bg-black">
+
+                        </div>
+                        <div className="Dashboard w-full h-1/6 bg-black">
+
+                        </div>
+                        <div className="Pages w-full h-1/6 bg-black">
+
+                        </div>
+                        <div className="Assets w-full h-1/6 bg-black">
+
+                        </div>
+                        <div className="Contracts w-full h-1/6 bg-black">
+
+                        </div>
+                        <div className="Settings w-full h-1/6 bg-black">
+
+                        </div>
+                    </div>
+
+                    {/* Dark Mode & Dashboard Version */}
+                    <div className="w-full h-1/8">
+
+                    </div>
+                </div>
 		</div>
 	);
 };
