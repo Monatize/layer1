4e4967dfb6d23c329f184c396f235397d1dfd4bf
TheLDB <ldb@erikboles.com>
2022-06-18 01:56:42 -0600

    making new sidebar that is reactive and shi yuh

---
+++ components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import { IconContext } from "react-icons";
+import { RiHome2Line, RiHome2Fill } from "react-icons/ri";
+import { MdOutlineDashboard, MdDashboard } from "react-icons/md";
+import { BsSunFill, BsMoonFill } from "react-icons/bs";
+
+interface ISidebar {
+	tab: string;
+	dark: boolean; // * True = dark False = light
+}
+
+const Sidebar = (props: ISidebar) => {
+	return (
+		<div
+			className={`w-full h-full md:rounded-3xl flex flex-col shrink-0 overflow-y-scroll ${
+				props.dark ? "gradient-border shadow-lg shadow-brand-green" : "gradient-border-light shadow-lg shadow-brand-purple"
+			}`}>
+			<div className="w-full pt-8 pb-8 flex shrink-0 justify-center items-center">
+				{props.dark && <img src="/m-white.png" className="w-16" />}
+				{!props.dark && <img src="/m-grad.png" className="w-16" />}
+			</div>
+			<div className="Spacer w-full md:pt-8 md:pb-8 flex shrink-0" />
+			<div className={`Home w-full pt-2 pb-2 flex justify-center items-center shrink-0 pl-2 pr-2`}>
+                <div className="w-full h-full flex justify-center items-center shrink-0 border-2 border-brand-green">
+                    <IconContext.Provider value={{size: '3rem', className: `${props.tab === "home" ? 'fill-brand-black' : 'fill-brand-black'}`}}>
+						<RiHome2Fill />
+					</IconContext.Provider>
+                </div>
+            </div>
+		</div>
+	);
+};
+
+export default Sidebar;
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,11 +1,21 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import Sidebar from "../../components/Sidebar";
 
 const Home: NextPage = () => {
-    return (
-        <div className="w-screen h-screen">
+	const [dark, setDark] = useState(true);
 
-        </div>
-    )
-}
+	return (
+		<div className={`w-screen h-screen ${dark ? 'bg-brand-black' : 'bg-brand-font'}`}>
+			<div className="w-full h-full flex flex-row shrink-0">
+				<div className={`w-[15rem] md:pt-8 md:pb-8 md:pl-12 md:pr-12`}>
+					<Sidebar tab={"home"} dark={dark}/>
+				</div>
+				<div className={`w-[calc(100%-15rem)] ${dark ? 'bg-brand-black' : 'bg-brand-font'} flex justify-center items-center`}>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Home;
