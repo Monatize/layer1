aded6b3025c7f069f86c9d1d6ca85e37b5880400
TheLDB <ldb@erikboles.com>
2022-06-18 02:10:08 -0600

    the sidebar needs responsive work

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = (props: ISidebar) => {
 	return (
 		<div
 			className={`w-full sm:rounded-3xl flex flex-col shrink-0 ${
-				props.dark ? "gradient- shadow-lg shadow-brand-green" : "gradient--light shadow-lg shadow-brand-purple"
+				props.dark ? "gradient-border shadow-lg shadow-brand-green" : "gradient-border-light shadow-lg shadow-brand-purple"
 			}`}>
 			<div className="w-full pt-8 pb-8 flex shrink-0 justify-center items-center">
 				{props.dark && <img src="/m-white.png" className="w-16" />}
@@ -20,7 +20,7 @@ const Sidebar = (props: ISidebar) => {
 			</div>
 			<div className="Spacer w-full md:pt-8 md:pb-8 flex shrink-0" />
 			<div className={`Home w-full pt-2 pb-2 flex justify-center items-center shrink-0 pl-2 pr-2`}>
-                <div className="w-full h-full flex justify-center items-center shrink-0 border-2 border-brand-green">
+                <div className="w-full flex justify-center items-center shrink-0 border-2 border-brand-green">
                     <IconContext.Provider value={{size: '3rem', className: `${props.tab === "home" ? 'fill-brand-black' : 'fill-brand-black'}`}}>
 						<RiHome2Fill />
 					</IconContext.Provider>
