8d529dad82b56dc41e29925ae43ed3e0a8a00d61
TheLDB <ldb@erikboles.com>
2022-07-08 20:47:51 -0600

    added new banner.png

--- components/Pages/PagesPanel.tsx
+++ components/Pages/PagesPanel.tsx
@@ -6,7 +6,7 @@ const PagesPanel = (props: IPagesPanel) => {
 	return (
 		<div className={`w-full h-full transition-all duration-500 overflow-y-scroll rounded-tl-2xl ${props.dark ? "gradient-border-tl" : "gradient-border-light-tl"}`}>
 			<div className="w-full h-full"></div>
-		</div>
+		</div>	
 	);
 };
 
