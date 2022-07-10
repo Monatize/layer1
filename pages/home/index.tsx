e2482e5f8e76ff0e5e5c64e1919b3b8a236ae1b0
TheLDB <ldb@erikboles.com>
2022-07-10 04:39:55 -0600

    making some design changes

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,5 +1,6 @@
 // * React/Next
 import { NextPage } from "next";
+
 import Head from "next/head";
 import { useEffect, useState } from "react";
 // * React/Next
@@ -22,67 +23,42 @@ import { BsMoonFill, BsSunFill } from "react-icons/bs";
 // * Icons
 
 const Home: NextPage = () => {
-	const [dark, setDark] = useState(false);
-	const [tab, setTab] = useState("stats");
+  const [dark, setDark] = useState(true);
+  const [tab, setTab] = useState("home");
 
-	const getLastTheme = () => {
-		const lsTheme = localStorage.getItem("dark");
-		if (lsTheme && lsTheme === "false") {
-			setDark(false);
-		} else {
-			setDark(true);
-		}
-	};
-	const toggleTheme = () => {
-		if (dark) {
-			localStorage.setItem("dark", "false");
-		} else {
-			localStorage.setItem("dark", "true");
-		}
-		setDark(!dark);
-	};
+  const getLastTheme = () => {
+    const lsTheme = localStorage.getItem("dark");
+    if (lsTheme && lsTheme === "false") {
+      setDark(true);
+    } else {
+      setDark(true);
+    }
+  };
+  const toggleTheme = () => {
+    if (dark) {
+      localStorage.setItem("dark", "false");
+    } else {
+      localStorage.setItem("dark", "true");
+    }
+    setDark(!dark);
+  };
 
-	const setNewTab = (tab: string) => {
-		setTab(tab);
-	};
+  const setNewTab = (tab: string) => {
+    setTab(tab);
+  };
 
-	useEffect(() => {
-		getLastTheme();
-	}, []);
-	return (
-		<div className={`w-screen h-screen ${dark ? "bg-brand-black" : "bg-brand-font"}`}>
-			<Head>
-				<title>Home</title>
-			</Head>
-			<div className="w-full h-full flex flex-row shrink-0">
-				<div className={`w-[10rem] h-full shrink-0 hidden sm:flex`}>
-					<Sidebar toggleTheme={toggleTheme} tab={tab} dark={dark} setTab={setNewTab} />
-				</div>
-				<div className={`w-full sm:w-[calc(100%-10rem)] transition-all ease-in-out duration-500 ${dark ? "bg-brand-black" : "bg-brand-font"}`}>
-					<div className={`w-full flex h-[6rem]`}>
-						<div className="w-1/2"></div>
-						<div className="w-1/2 flex space-x-4 justify-end items-center pr-4">
-							<IconContext.Provider
-								value={{
-									className: `${dark ? "fill-brand-font" : "fill-brand-black"} hover:cursor-pointer`,
-									size: `2.5rem`,
-								}}>
-								{dark && <BsSunFill onClick={toggleTheme} />}
-								{!dark && <BsMoonFill onClick={toggleTheme} />}
-							</IconContext.Provider>
-							<div className="w-16 h-16 bg-gray-500 flex justify-center items-center rounded-full text-white font-bold text-3xl">P</div>
-						</div>
-					</div>
-					<div className="w-full h-[calc(100%-6rem)] overflow-y-scroll">
-						{tab === "home" && <HomePanel dark={dark} />}
-						{tab === "stats" && <StatsPanel dark={dark} />}
-						{tab === "pages" && <PagesPanel dark={dark} />}
-						{tab === "contracts" && <ContractsPanel dark={dark} />}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    getLastTheme();
+  }, []);
+  return (
+    <div className="w-screen h-screen flex">
+      <div className={`w-[15rem] h-full p-8 ${dark ? 'bg-brand-black' : 'bg-brand-soft-white'}`}>
+        <div className={`w-full h-full border ${dark ? 'border-brand-soft-white shadow-lg shadow-light-btn' : 'border-brand-soft-white shadow-light-btn'}`}>
+        </div>
+      </div>
+      <div className={`w-[calc(100%-15rem)] ${dark ? 'bg-brand-black' : 'bg-brand-soft-white'}`}>
+      </div>
+    </div>
+  );
 };
-
 export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-	return (
-		<div className="w-screen h-screen">
-			<Head>
-				<title>Monatize Dashboard</title>
-			</Head>
-			<main className="w-full h-full">
-				<div className="w-full h-full bg-green-500"></div>
-			</main>
-		</div>
-	);
+  return (
+    <div className="w-screen h-screen">
+      <Head>
+        <title>Monatize Dashboard</title>
+      </Head>
+      <main className="w-full h-full">
+        <div className="w-full h-full bg-blue-500"></div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
