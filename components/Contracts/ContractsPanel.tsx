ff53dcf00c91164f37dd1f07e8d1a4add28c34f1
TheLDB <ldb@erikboles.com>
2022-07-22 06:18:19 -0600

    Added id key to each news post

--- components/Contracts/ContractsPanel.tsx
+++ components/Contracts/ContractsPanel.tsx
@@ -4,10 +4,9 @@ interface IContractsPanel {
 
 const ContractsPanel = (props: IContractsPanel) => {
 	return (
-		<div className="w-full h-full bg-brand-black flex flex-col justify-center items-center">
-		<h1 className="text-brand-soft-white font-sans font-bold text-8xl">Contracts</h1>
-		<h1 className="text-brand-soft-white font-sans font-semibold text-5xl">(coming soon)</h1>
-	</div>
+		<div className={`w-full h-full`}>
+			
+		</div>
 	);
 };
 
